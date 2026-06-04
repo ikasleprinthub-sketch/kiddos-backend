eb6fa056fb346c850dd1e8f0da232cee0dc55dff
ikasleprinthub-sketch <ikasleprinthub@gmail.com>
2026-06-04 06:55:36 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "pg", "razorpay"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
   async headers() {
     return [
       {
